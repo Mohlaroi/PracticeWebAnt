7bdf274e29b0c108467b44b60d126cba0952ea48
Mohlaroi <mohlaroisabirova13@gmail.com>
2025-06-22 02:18:20 +0300

    Delete практика 3  directory

--- практика 3 /src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-  <footer className="footer">
-    <p>Make with ❤️ for the MobProgramming team</p>
-  </footer>
-);
-
-export default Footer;
--- практика 3 /src/components/LoadMoreButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const LoadMoreButton = ({ onClick }) => (
-  <button className="loadmore" onClick={onClick}>
-    LOAD MORE
-  </button>
-);
-
-export default LoadMoreButton;
